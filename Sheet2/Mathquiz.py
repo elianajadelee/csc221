2ff79287a27900c5da22e3ec8fc69efd1238636e
Eliana Lee <rockie.jlee07@gmail.com>
2023-07-06 09:26:55 -0400

    Update repo

--- Sheet2/Mathquiz.py
+++ Sheet2/Mathquiz.py
@@ -4,10 +4,10 @@ x = int(input('Tell me how many question you want to solve? '))
 for x in range(x):
     num1 = randint(1, 20)
     num2 = randint (1, 20)
-    question = "What is " str(num1) + " times " + str(num2) + "? "
+    question = "What is " + str(num1) + " times " + str(num2) + "? "
     answer = int(input(question))
     product = num1 * num2
- if answer == product:
+    if answer == product:
         print('Yes, the answer is ' + str(product) + '!')
         print('Well done you answered correctly!')
         right = right +1
@@ -15,5 +15,5 @@ for x in range(x):
         print('Sorry that is incorrect!')
         print('The correct answer is ' + str(product) + '.')
 x = x + 1
-print(" I asked you " + str(x) + "questions. You got " + str(right) + ".")
+print(" I asked you "  + str(x)  + "questions. You got " + str(right) + ".")
 print("Well done!")
