5bbb9e70fa028e093f03ccdd32c6c9526ce951c7
Eliana Lee <rockie.jlee07@gmail.com>
2023-07-10 10:21:09 -0400

    Challenge1

--- Sheet2/Sheet3/SmileyFace.py
+++ Sheet2/Sheet3/SmileyFace.py
@@ -2,17 +2,17 @@ from gasp import*
 
 begin_graphics()
 
-Circle((300,200),50)
+Circle((100,200),50)
 
-Circle((285, 210), 5)
+Circle((85, 210), 5)
 
-Circle((315, 210), 5)
+Circle((115, 210), 5)
 
-Line((300,210), (290, 190))
+Line((100,210), (90, 190))
 
-Line((290,190), (310, 190))
+Line((90,190), (110, 190))
 
-Arc((300, 200), 30, 225, 315)
+Arc((100, 200), 30, 225, 315)
 
 update_when('key_pressed')
 end_graphics()
