3c0471d525ca2b968a2b3eb9d79f026db2f81d06
Eliana Lee <rockie.jlee07@gmail.com>
2023-07-10 11:02:09 -0400

    Challenge4

--- Sheet2/Sheet3/Functions.py
+++ Sheet2/Sheet3/Functions.py
@@ -2,3 +2,9 @@ def moan():
     print("python is useless!")
     print("And so are these worksheets")
 moan()
+
+def squaring(x):
+    print(x*x)
+
+y = int(input("Enter the number you want squared"))
+squaring(y)
