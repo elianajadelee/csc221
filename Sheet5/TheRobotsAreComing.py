e3c7d3c1494bffc6baf35e34f4a4fa8fb3c5e3b7
Eliana Lee <rockie.jlee07@gmail.com>
2023-07-18 09:19:23 -0400

    MovingDots

--- Sheet5/TheRobotsAreComing.py
+++ Sheet5/TheRobotsAreComing.py
@@ -2,6 +2,7 @@ from gasp import*
 
 begin_graphics()
 finished = False 
+
 c = Circle((320, 200), 5)
 move_to(c, (300, 220))
 
@@ -15,10 +16,26 @@ from random import randint
 player_x = randint(0,63)
 player_y = randint(0,47)
 
-Circle((10* player_x + 5, 10* player_y + 5), 5, filled = True)
-
-while not finished:
-    move_player
+while True: 
+   
+    player = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
     
+    key = update_when('key_pressed')
+   
+    remove_from_screen(player)
+    
+    if key == 'Right':
+        player_x += 1
+    if key == 'Left':
+        player_x -= 1
+    if key == 'Up':
+        player_y += 1
+    if key == 'Down': 
+        player_y -= 1
+    if key == 'q':
+        break
+
+move_to(Circle, (10 * player_x + 5, 10 * player_y + 5))
+
 update_when('key_pressed')
 end_graphics()
